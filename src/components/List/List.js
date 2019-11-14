e9069cd5aa1b6e99592c41f299a6ee1f93a79943
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2019-11-14 22:54:59 +0100

    Add img props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js'
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  /* Define props types */
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    src: PropTypes.string,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} srcText={this.props.src} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
