664b00b1b86c5c64ada274ca9836b6655981e7a1
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2019-11-16 13:49:58 +0100

    Add addColumn

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
 
 
 class List extends React.Component {
@@ -24,6 +25,22 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   }
 
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
   render() {
     return (
       <section className={styles.component}>
@@ -35,6 +52,9 @@ class List extends React.Component {
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
           ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
