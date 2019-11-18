a901a0c87e0defc9bb448be1d55c2a01bc519a46
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2019-11-18 22:46:21 +0100

    Display cards in columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
-import {settings} from '../../data/dataStore';
+//import Creator from '../Creator/Creator.js';
+//import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || [],
-  }
 
   /* Define props types */
   static propTypes = {
@@ -20,22 +17,9 @@ class Column extends React.Component {
     icon: PropTypes.string.isRequired,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-          },
-        ],
-      }
-    ));
-  }
 
   render() {
+    const {cards} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
@@ -45,13 +29,14 @@ class Column extends React.Component {
           </span>
         </h3>
         <div className={styles.cards}>
-          {this.state.cards.map(({key, ...cardProps}) => (
-            <Card key={key} {...cardProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
+        {/*
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div>
+        </div>*/}
       </section>
     );
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,13 +7,13 @@ import styles from './Hero.scss';
 const Hero = props => (
   <header className={styles.component}>
     <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
-    <img className={styles.image} src={props.srcText}/>
+    <img className={styles.image} src={props.image}/>
   </header>
 );
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  srcText: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Hero;
